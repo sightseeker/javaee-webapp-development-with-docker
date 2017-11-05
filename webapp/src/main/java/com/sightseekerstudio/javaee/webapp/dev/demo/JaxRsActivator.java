b3478201b9c89789969fc2cecb3a527e0a04fb30
SightSeeker <master@sightseekerstudio.com>
2017-11-06 08:32:30 +0900

    create jaxrs resource

---
+++ webapp/src/main/java/com/sightseekerstudio/javaee/webapp/dev/demo/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package com.sightseekerstudio.javaee.webapp.dev.demo;
+
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+
+}
